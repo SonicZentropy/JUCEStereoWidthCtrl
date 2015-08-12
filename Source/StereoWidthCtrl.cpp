f9c606622341324fac8765f439a677598f458451
Casey <SonicZentropy@gmail.com>
2015-08-12 02:35:33 -0500

    Added StereoWidthCtrl

---
+++ Source/StereoWidthCtrl.cpp
@@ -0,0 +1,45 @@
+/*
+  ==============================================================================
+
+    StereoWidthCtrl.cpp
+    Created: 12 Aug 2015 1:38:56am
+    Author:  Zentropy
+
+  ==============================================================================
+*/
+
+#include "StereoWidthCtrl.h"
+
+StereoWidthCtrl::StereoWidthCtrl()
+{
+	setWidth(1.0f);
+}
+
+StereoWidthCtrl::~StereoWidthCtrl()
+{
+	
+}
+
+void StereoWidthCtrl::setWidth(float width)
+{
+	stereoWidthValue = width;
+	float temp;
+	if (1.0f + width > 2.0f)
+	{
+		temp = 1.0f / (1.0f + stereoWidthValue);
+	}
+	else temp = 1.0f / 2.0f;
+	diffGain = stereoWidthValue * temp;  ///diffGain is gain coefficient for Side 
+	sumGain = temp;  ///sumGain is gain coefficient for Mid
+}
+
+void StereoWidthCtrl::ClockProcess(float* LeftSample, float* RightSample)
+{
+	float mid = sumGain * (*LeftSample + *RightSample);
+	float side = diffGain * (*RightSample - *LeftSample);
+	*LeftSample = mid - side;
+	*RightSample = mid + side;
+}
+
+
+
---
+++ Source/StereoWidthCtrl.h
@@ -0,0 +1,43 @@
+/*
+  ==============================================================================
+
+    StereoWidthCtrl.h
+    Created: 12 Aug 2015 1:39:08am
+    Author:  Zentropy
+	Volume-Normalized Stereo Width Control - DSP from Michael Gruhn
+	'width' is stretch factor of stereo field:
+	width < 1: decrease stereo width
+	width = 1: no change
+	width > 1: increase width
+	width = 0: MONO conversion
+
+  ==============================================================================
+*/
+
+#ifndef STEREOWIDTHCTRL_H_INCLUDED
+#define STEREOWIDTHCTRL_H_INCLUDED
+
+
+
+
+
+#endif  // STEREOWIDTHCTRL_H_INCLUDED
+
+class StereoWidthCtrl
+{
+public:
+	StereoWidthCtrl();
+	~StereoWidthCtrl();
+
+	//Parms
+	void setWidth(float width);
+	float getWidth(void) { return stereoWidthValue; };
+
+	// Use
+	void ClockProcess(float* LeftSample, float* RightSample);
+
+private:
+	float stereoWidthValue, sumGain, diffGain;
+
+
+};
