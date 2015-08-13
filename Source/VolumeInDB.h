8cd7f212e73044a86f7fe55b9c6c7f6a6a4c8360
Casey <SonicZentropy@gmail.com>
2015-08-13 07:17:50 -0500

    Branch for Volume in DB

---
+++ Source/VolumeInDB.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    VolumeInDB.h
+    Created: 13 Aug 2015 5:57:08am
+    Author:  Zentropy
+
+  ==============================================================================
+*/
+
+#ifndef VOLUMEINDB_H_INCLUDED
+#define VOLUMEINDB_H_INCLUDED
+
+
+
+
+
+#endif  // VOLUMEINDB_H_INCLUDED
