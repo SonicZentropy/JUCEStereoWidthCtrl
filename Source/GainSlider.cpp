19ccff80fda7dd904c1db3d5afd45d0ef6b8c2ee
Zentropy <SonicZentropy@gmail.com>
2015-08-14 06:49:30 -0500

    Added files

---
+++ Source/GainSlider.cpp
@@ -0,0 +1,23 @@
+/*
+  ==============================================================================
+
+    gainSlider.cpp
+    Created: 14 Aug 2015 4:48:22am
+    Author:  Zentropy
+
+  ==============================================================================
+*/
+
+#include "gainSlider.h"
+
+
+// Override Slider::getTextFromValue to force value to -inf display
+String GainSlider::getTextFromValue(double value)
+{
+
+	if (value <= -95.0f) return String("-inf");
+	if (getNumDecimalPlacesToDisplay() > 0)
+		return String(value, getNumDecimalPlacesToDisplay()) + getTextValueSuffix();
+
+	return String(roundToInt(value)) + getTextValueSuffix();
+}
---
+++ Source/GainSlider.h
@@ -0,0 +1,26 @@
+/*
+  ==============================================================================
+
+    gainSlider.h
+    Created: 14 Aug 2015 4:48:22am
+    Author:  Zentropy
+
+  ==============================================================================
+*/
+
+#ifndef GAINSLIDER_H_INCLUDED
+#define GAINSLIDER_H_INCLUDED
+
+
+
+
+
+#endif  // GAINSLIDER_H_INCLUDED
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class GainSlider : public Slider
+{
+public:
+	GainSlider(const String& componentName) : Slider(componentName) {};
+	String getTextFromValue(double value) override;
+};
