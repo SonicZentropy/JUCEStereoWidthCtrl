6bce18d564e56e619d21644d30ce9d14502ff9dc
Zentropy <SonicZentropy@gmail.com>
2015-08-14 11:43:51 -0500

    Added Utility files

---
+++ Source/ParamUtils.h
@@ -0,0 +1,14 @@
+/*
+  ==============================================================================
+
+    ParamUtil.h
+    Created: 14 Aug 2015 7:44:55am
+    Author:  Zentropy
+
+  ==============================================================================
+*/
+
+#ifndef PARAMUTIL_H_INCLUDED
+#define PARAMUTIL_H_INCLUDED
+
+#endif  // PARAMUTIL_H_INCLUDED
